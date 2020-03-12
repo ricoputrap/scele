51e793859ea4af8ce3185ce72640f15de802bd98
rico.putra <rico.putra@outlook.co.id>
2020-03-12 09:58:32 +0700

    register models in admin

--- sceleapp/admin.py
+++ sceleapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import UserApp, Course, UserPost, PostLike, GivenPostLike, UserReply, ReplyLike, GivenReplyLike, Notif, Badge, UserBadge
 
 # Register your models here.
+admin.site.register(UserApp)
+admin.site.register(Course)
+admin.site.register(UserPost)
+admin.site.register(PostLike)
+admin.site.register(GivenPostLike)
+admin.site.register(UserReply)
+admin.site.register(ReplyLike)
+admin.site.register(GivenReplyLike)
+admin.site.register(Notif)
+admin.site.register(Badge)
+admin.site.register(UserBadge)
