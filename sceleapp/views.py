26e2bde9305a410a086fd9b2d767fb7d7d9e36bb
rico.putra <rico.putra@outlook.co.id>
2020-03-17 16:46:24 +0700

    initiate register web skeleton

--- sceleapp/views.py
+++ sceleapp/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login, authenticate
+from django.shortcuts import render, redirect
+
+from sceleapp.forms import RegisterForm
 
 # Create your views here.
+is_logged_in = False
+
 def login(request):
     return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            uname = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=uname, password=raw_password)
+            auth_login(request, user)
+            registered = True
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
