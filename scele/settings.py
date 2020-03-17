482f21f8204a173aa80b3cb3a99f7421e6cd45d4
rico.putra <rico.putra@outlook.co.id>
2020-03-17 23:37:56 +0700

    prepare header

--- scele/settings.py
+++ scele/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'sceleapp/static')
 STATIC_URL = '/static/' # we can access static files with '/static/' keyword in URL
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+ # Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
