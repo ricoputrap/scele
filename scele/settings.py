3b08523b2a5f9cdfa1e016723c9537e041b98eb0
rico.putra <rico.putra@outlook.co.id>
2020-03-18 14:48:00 +0700

    fixed staticfiles configuration

--- scele/settings.py
+++ scele/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'sceleapp/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/' # we can access static files with '/static/' keyword in URL
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'sceleapp/static'),
 )
  # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
