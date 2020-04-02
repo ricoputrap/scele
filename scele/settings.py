2f7584ef175f5ae9cdb75c2a87924429b4289b40
rico.putra <rico.putra@outlook.co.id>
2020-04-02 23:21:01 +0700

    configure deployment

--- scele/settings.py
+++ scele/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PRODUCTION = os.environ.get('DATABASE_URL') != None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+# SECRET_KEY = '8f($8+jserccc6j99igr#b3%dgi8wx!5fu2%fe-t-k%(3=@wk'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '8f($8+jserccc6j99igr#b3%dgi8wx!5fu2%fe-t-k%(3=@wk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
@@ -82,6 +85,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+if PRODUCTION:
+    DATABASES['default'] = dj_database_url.config()
+
 
 
 # Password validation
